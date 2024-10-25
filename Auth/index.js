e802677fd28a3901a9b581233dadda25ef662323
Naren <deadlyassassin2003@gmail.com>
2024-10-25 15:40:04 +0530

    adding profile routes to the index.js

--- Auth/index.js
+++ Auth/index.js
@@ -17,6 +17,81 @@ app.use(express.json())
 app.use('/individual/',authIndividualRouter)
 app.use('/enterprise/',authEnterpriseRouter)
 
+
+const { MongoClient } = require ("mongodb");
+const dotenv = require ("dotenv");
+
+dotenv.config();
+app.use(express.json({ limit: "10mb" }));
+
+const uri = process.env.MongoDBURL || "";
+const client = new MongoClient(uri);
+
+app.get("/api/profile", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("diskuss");
+    const collection = database.collection("profile");
+    const documents = await collection.find({}).toArray();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get", error });
+  } finally {
+    await client.close();
+  }
+});
+
+app.post("/api/profile", async (req, res) => {
+  const {
+    businessName,
+    yourName,
+    designation,
+    mobile,
+    email,
+    location,
+    services,
+    image,
+    position,
+    color,
+  } = req.body;
+
+  try {
+    await client.connect();
+    const database = client.db("diskuss");
+    const collection = database.collection("profile");
+
+    const newEntry = {
+      businessName,
+      yourName,
+      designation,
+      mobile,
+      email,
+      location,
+      services,
+      image,
+      position,
+      color,
+    };
+
+    const result = await collection.insertOne(newEntry);
+    res.json({
+      message: "Entry added successfully",
+      entryId: result.insertedId,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to add entry", error });
+  } finally {
+    await client.close();
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
 const port = process.env.PORT | "3000"
 app.listen(port ,()=>{
   console.log(`Server Connected port : ${port}`);
